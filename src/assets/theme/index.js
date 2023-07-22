18d65d99b8c3bc7ecd9bb2357b3b4be6b9a0d546
Xu <whitenight.xiaxu@gmail.com>
2023-07-22 15:29:49 +0800

    feat theme color

---
+++ src/assets/theme/index.js
@@ -0,0 +1,8 @@
+const theme = {
+    color: {
+        primary: "#ff385c",
+        second: "#00848A"
+    }
+}
+
+export default theme
--- src/components/app-header/c-cpns/header-left/sytle.js
+++ src/components/app-header/c-cpns/header-left/sytle.js
@@ -1,6 +1,13 @@
 import  styled from "styled-components"
 
 export const LeftWrapper = styled.div`
-    flex: 1;
-    color:red;
+  flex: 1;
+  color: ${(props => props.theme.color.primary)};
+  display: flex;
+  align-items: center;
+
+  .logo {
+    margin-left: 25px;
+    cursor: pointer;
+  }
 `
