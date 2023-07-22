7224184de7eee8aa61c480b27e8f7bd1de9d289b
Xu <whitenight.xiaxu@gmail.com>
2023-07-22 16:14:13 +0800

    feat header right

--- src/assets/theme/index.js
+++ src/assets/theme/index.js
@@ -2,6 +2,18 @@ const theme = {
     color: {
         primary: "#ff385c",
         second: "#00848A"
+    },
+    text: {
+        primary: "#484848",
+        second: "#222"
+    },
+    mixin:{
+        boxShadow:`
+        transition: box-shadow 200ms ease;
+        &:hover {
+        box-shadow: 0 2px 4px rgba(0,0,0,0.18);
+        }
+        `
     }
 }
 
--- src/components/app-header/c-cpns/header-right/style.js
+++ src/components/app-header/c-cpns/header-right/style.js
@@ -2,7 +2,41 @@ import styled from "styled-components";
 
 export const RightWrapper = styled.div`
   flex: 1;
-  color: orange;
   display: flex;
   justify-content: flex-end;
-`;
+  align-items: center;
+  font-weight: 600;
+  color: ${props => props.theme.text.primary};
+
+  .btns {
+    display: flex;
+    align-items: center;
+    
+    .btn {
+      height: 18px;
+      line-height: 18px;
+      padding: 12px 15px;
+      cursor: pointer;
+      border-radius: 22px;
+      
+      &:hover {
+        background-color: #f5f5f5;
+      }
+    }
+  }
+
+  .profile {
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    margin-right: 24px;
+    width: 77px;
+    height: 42px;
+    border: 1px  solid #ccc;
+    background-color: #fff;
+    color: ${props => props.theme.text.primary};
+    cursor: pointer;
+    border-radius: 25px;
+    ${props => props.theme.mixin.boxShadow}
+  }
+`
