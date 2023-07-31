9e1d3bedba1eefde55474f51df0a4472e3400c70
Xu <whitenight.xiaxu@gmail.com>
2023-07-31 16:18:35 +0800

    feat home picture

---
+++ src/views/home/cpns/home-banner/index.jsx
@@ -0,0 +1,8 @@
+import React, { memo } from 'react'
+import { BannerWrapper } from '@/views/home/cpns/home-banner/style'
+
+const HomeBanner = memo(() => {
+  return <BannerWrapper></BannerWrapper>
+})
+
+export default HomeBanner
--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,15 +1,14 @@
 import React, { memo, useEffect } from 'react'
 import xxRequest from '@/services'
+import HomeBanner from 'src/views/home/cpns/home-banner'
 
 const Home = memo(() => {
-    useEffect(() => {
-        xxRequest.get({url:"/home/highscore"}).then(res => {
-            console.log(res)
-        })
-    },[])
-  return (
-    <div>Home</div>
-  )
+  useEffect(() => {
+    xxRequest.get({ url: '/home/highscore' }).then((res) => {
+      console.log(res)
+    })
+  }, [])
+  return <HomeBanner />
 })
 
 export default Home
