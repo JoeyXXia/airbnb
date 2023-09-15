116ba2d85f25b0a883718a1808600e3409e15e3d
Xu <whitenight.xiaxu@gmail.com>
2023-09-15 17:06:17 +0800

    feat home store

--- src/store/modules/home.js
+++ src/store/modules/home.js
@@ -1,30 +1,80 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
-import { getHomeGoodPriceData } from '@/services/modules/home'
+import {
+  getHomeDiscountData,
+  getHomeGoodPriceData,
+  getHomeHighScoreData,
+  getHomeHotRecommendData,
+  getHomeLongForData,
+  getHomePlusData
+} from '@/services/modules/home'
 
 export const fetchHomeDataAction = createAsyncThunk(
   'fetchdata',
-  async (payload) => {
-    const res = await getHomeGoodPriceData()
-    return res
+  (payload, { dispatch }) => {
+    getHomeGoodPriceData().then((res) => {
+      dispatch(changeGoodPriceInfoAction(res))
+    })
+    getHomeHighScoreData().then((res) => {
+      dispatch(changeHighScoreInfoAction(res))
+    })
+    getHomeDiscountData().then((res) => {
+      dispatch(changeDiscountInfoAction(res))
+    })
+    getHomeHotRecommendData().then((res) => {
+      dispatch(changeRecommendInfoAction(res))
+    })
+    getHomeLongForData().then((res) => {
+      dispatch(changeLongForInfoAction(res))
+    })
+    getHomePlusData().then((res) => {
+      dispatch(changePlusInfoAction(res))
+    })
   }
 )
 
 const homeSlice = createSlice({
   name: 'home',
   initialState: {
-    goodPriceInfo: {}
+    goodPriceInfo: {},
+    highScoreInfo: {},
+    discountInfo: {},
+    recommendInfo: {},
+    longForInfo: {},
+    plusInfo: {}
   },
   reducers: {
     changeGoodPriceInfoAction(state, { payload }) {
       state.goodPriceInfo = payload
+    },
+    changeHighScoreInfoAction(state, { payload }) {
+      state.highScoreInfo = payload
+    },
+    changeDiscountInfoAction(state, { payload }) {
+      state.discountInfo = payload
+    },
+    changeRecommendInfoAction(state, { payload }) {
+      state.recommendInfo = payload
+    },
+    changeLongForInfoAction(state, { payload }) {
+      state.longForInfo = payload
+    },
+    changePlusInfoAction(state, { payload }) {
+      state.plusInfo = payload
     }
   },
   extraReducers: {
-    [fetchHomeDataAction.fulfilled](state, { payload }) {
-      state.goodPriceInfo = payload
-    }
+    // [fetchHomeDataAction.fulfilled](state, { payload }) {
+    //   state.goodPriceInfo = payload
+    // }
   }
 })
 
-export const { changeGoodPriceInfoAction } = homeSlice.actions
+export const {
+  changeGoodPriceInfoAction,
+  changeHighScoreInfoAction,
+  changeDiscountInfoAction,
+  changeRecommendInfoAction,
+  changeLongForInfoAction,
+  changePlusInfoAction
+} = homeSlice.actions
 export default homeSlice.reducer
