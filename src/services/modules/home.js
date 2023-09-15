de32e77e40b609be7fcebf48905d9cb26a3eba2e
Xu <whitenight.xiaxu@gmail.com>
2023-09-15 16:44:53 +0800

    feat home network request

--- src/services/modules/home.js
+++ src/services/modules/home.js
@@ -5,3 +5,33 @@ export function getHomeGoodPriceData() {
     url: '/home/goodprice'
   })
 }
+
+export function getHomeHighScoreData() {
+  return xxRequest.get({
+    url: '/home/highscore'
+  })
+}
+
+export function getHomeDiscountData() {
+  return xxRequest.get({
+    url: '/home/discount'
+  })
+}
+
+export function getHomeHotRecommendData() {
+  return xxRequest.get({
+    url: '/home/hotrecommenddest'
+  })
+}
+
+export function getHomeLongForData() {
+  return xxRequest.get({
+    url: '/home/longfor'
+  })
+}
+
+export function getHomePlusData() {
+  return xxRequest.get({
+    url: '/home/plus'
+  })
+}
