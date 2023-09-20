0e62eac15345fa1360c796b8c4d4ca07e7f7a458
Xu <whitenight.xiaxu@gmail.com>
2023-09-20 15:50:15 +0800

    fix style error

--- src/components/room-item/style.js
+++ src/components/room-item/style.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 export const ItemWrapper = styled.div`
   box-sizing: border-box;
-  width: 25%;
+  width: ${(props) => props.itemWidth};
   padding: 8px;
 
   .inner {
--- src/components/section-rooms/index.jsx
+++ src/components/section-rooms/index.jsx
@@ -4,11 +4,11 @@ import RoomItem from '@/components/room-item'
 import PropTypes from 'prop-types'
 
 const SectionRooms = memo((props) => {
-  const { roomList = [] } = props
+  const { roomList = [], itemWidth } = props
   return (
     <RoomsWrapper>
       {roomList.slice(0, 8)?.map((item) => {
-        return <RoomItem itemData={item} key={item.id} />
+        return <RoomItem itemData={item} key={item.id} itemWidth={itemWidth} />
       })}
     </RoomsWrapper>
   )
