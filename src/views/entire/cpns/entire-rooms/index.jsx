7502cd9c19926ab30d044b9f4a77bfee51029d33
Xu <whitenight.xiaxu@gmail.com>
2023-10-11 14:53:47 +0800

    feature entire rooms

--- src/views/entire/cpns/entire-rooms/index.jsx
+++ src/views/entire/cpns/entire-rooms/index.jsx
@@ -1,8 +1,42 @@
 import React, { memo } from 'react'
 import { EntireRoomWrapper } from '@/views/entire/cpns/entire-rooms/style'
+import { shallowEqual, useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import RoomItem from '@/components/room-item'
 
 const EntireRooms = memo(() => {
-  return <EntireRoomWrapper>i am entire rooms</EntireRoomWrapper>
+  const { roomList, isLoading } = useSelector(
+    (state) => ({
+      roomList: state.entire.roomList,
+      isLoading: state.entire.isLoading
+    }),
+    shallowEqual
+  )
+
+  const naviggate = useNavigate()
+  const dispatch = useDispatch()
+
+  function handleItemClick(item) {
+    naviggate('/detail')
+    // dispatch(ch)
+  }
+  return (
+    <EntireRoomWrapper>
+      <div className="list">
+        {roomList.map((item, index) => {
+          return (
+            <RoomItem
+              itemData={item}
+              itemWidth="20%"
+              key={item._id}
+              itemClick={(e) => handleItemClick(item)}
+            />
+          )
+        })}
+      </div>
+      {isLoading && <div className="cove"></div>}
+    </EntireRoomWrapper>
+  )
 })
 
 export default EntireRooms
--- src/views/entire/cpns/entire-rooms/style.js
+++ src/views/entire/cpns/entire-rooms/style.js
@@ -1,2 +1,19 @@
 import styled from 'styled-components'
-export const EntireRoomWrapper = styled.div``
+export const EntireRoomWrapper = styled.div`
+  position: relative;
+  padding: 30px 20px;
+
+  .list {
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  > .cover {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    background-color: rgba(255, 255, 255, 0.8);
+  }
+`
