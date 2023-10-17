24806b034bc8569f4e34dc6b124a6be069f7d975
Xu <whitenight.xiaxu@gmail.com>
2023-10-17 15:30:53 +0800

    indicator component

--- src/base-ui/indicator/style.js
+++ src/base-ui/indicator/style.js
@@ -1,3 +1,14 @@
 import styled from 'styled-components'
 
-export const IndicatorWrapper = styled.div``
+export const IndicatorWrapper = styled.div`
+  overflow: hidden;
+  .scroll {
+    position: relative;
+    display: flex;
+    transition: transform 200ms ease;
+
+    > * {
+      flex-shrink: 0;
+    }
+  }
+`
