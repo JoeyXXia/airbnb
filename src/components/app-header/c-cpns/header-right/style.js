07f255a53b80b751d6b9a5d91d4f47dc1b0606c4
Xu <whitenight.xiaxu@gmail.com>
2024-04-18 16:07:19 +0800

    update style format

--- src/components/app-header/c-cpns/header-right/style.js
+++ src/components/app-header/c-cpns/header-right/style.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const RightWrapper = styled.div`
   flex: 1;
@@ -6,19 +6,19 @@ export const RightWrapper = styled.div`
   justify-content: flex-end;
   align-items: center;
   font-weight: 600;
-  color: ${props => props.theme.text.primary};
+  color: ${(props) => props.theme.text.primary};
 
   .btns {
     display: flex;
     align-items: center;
-    
+
     .btn {
       height: 18px;
       line-height: 18px;
       padding: 12px 15px;
       cursor: pointer;
       border-radius: 22px;
-      
+
       &:hover {
         background-color: #f5f5f5;
       }
@@ -27,18 +27,19 @@ export const RightWrapper = styled.div`
 
   .profile {
     display: flex;
+    position: relative;
     align-items: center;
     justify-content: space-evenly;
     margin-right: 24px;
     width: 77px;
     height: 42px;
-    border: 1px  solid #ccc;
+    border: 1px solid #ccc;
     background-color: #fff;
-    color: ${props => props.theme.text.primary};
+    color: ${(props) => props.theme.text.primary};
     cursor: pointer;
     border-radius: 25px;
-    ${props => props.theme.mixin.boxShadow};
-    
+    ${(props) => props.theme.mixin.boxShadow};
+
     .panel {
       position: absolute;
       top: 68px;
@@ -46,17 +47,18 @@ export const RightWrapper = styled.div`
       width: 240px;
       background-color: #fff;
       border-radius: 10px;
-      box-shadow: 0 0 6px rgba(0,0,0,.2);
+      box-shadow: 0 0 6px rgba(0, 0, 0, 0.2);
       color: #666;
-      
-      .top, .bottom {
+
+      .top,
+      .bottom {
         padding: 10px 0;
-        
+
         .item {
           height: 40px;
           line-height: 40px;
           padding: 0 16px;
-          
+
           &:hover {
             background-color: #f5f5f5;
           }
