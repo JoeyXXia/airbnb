150aa37275e376e4d8ac78aff971c682629bde11
Xu <whitenight.xiaxu@gmail.com>
2024-06-04 09:29:31 +0800

    add strict mode

--- craco.config.js
+++ craco.config.js
@@ -1,17 +1,20 @@
 const path = require('path')
 const CracoLessPlugin = require('craco-less')
 
-const resolve = pathname => path.resolve(__dirname,pathname)
+const resolve = (pathname) => path.resolve(__dirname, pathname)
 
 module.exports = {
-    plugins:[{
-        plugin:CracoLessPlugin
-    }],
-    webpack: {
-        alias: {
-            "@":resolve("src"),
-            "components": resolve("src/components"),
-            "utils" : resolve("src/utils")
-        }
+  mode: 'development',
+  plugins: [
+    {
+      plugin: CracoLessPlugin
     }
+  ],
+  webpack: {
+    alias: {
+      '@': resolve('src'),
+      components: resolve('src/components'),
+      utils: resolve('src/utils')
+    }
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import { Suspense } from 'react'
@@ -13,13 +13,15 @@ import theme from '@/assets/theme'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <Suspense fallback="loading">
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <HashRouter>
-          <App />
-        </HashRouter>
-      </ThemeProvider>
-    </Provider>
-  </Suspense>
+  <StrictMode>
+    <Suspense fallback="loading">
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <HashRouter>
+            <App />
+          </HashRouter>
+        </ThemeProvider>
+      </Provider>
+    </Suspense>
+  </StrictMode>
 )
