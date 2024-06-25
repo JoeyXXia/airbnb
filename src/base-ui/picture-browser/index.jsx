6a3a1e750b91b767654a6598febd7b8509c45389
Xu <whitenight.xiaxu@gmail.com>
2024-06-25 11:02:52 +0800

    arrow change picture

--- src/base-ui/picture-browser/index.jsx
+++ src/base-ui/picture-browser/index.jsx
@@ -1,10 +1,14 @@
-import React, { memo, useEffect } from 'react'
+import React, { memo, useEffect, useState } from 'react'
 import PropTypes, { func } from 'prop-types'
 import { PictureBrowserWrapper } from '@/base-ui/picture-browser/style'
 import IconClose from '@/assets/svg/icon-close'
+import IconArrowLeft from '@/assets/svg/icon-arrow-left'
+import IconArrowRight from '@/assets/svg/icon-arrow-right'
 
 const PictureBrowser = memo((props) => {
   const { pictureUrls, closeClick } = props
+
+  const [currentIndex, setCurrentIndex] = useState(0)
 
   useEffect(() => {
     document.body.style.overflow = 'hidden'
@@ -17,6 +21,14 @@ const PictureBrowser = memo((props) => {
     if (closeClick) closeClick()
   }
 
+  function controlClickHandle(isNext = true) {
+    let newIndex = isNext ? currentIndex + 1 : currentIndex - 1
+    if (newIndex < 0) newIndex = pictureUrls.length - 1
+    if (newIndex > pictureUrls.length - 1) newIndex = 0
+
+    setCurrentIndex(newIndex)
+  }
+
   return (
     <PictureBrowserWrapper>
       <div className="top">
@@ -24,8 +36,26 @@ const PictureBrowser = memo((props) => {
           <IconClose />
         </div>
       </div>
-      <div className="list"></div>
-      <div className="indicator"></div>
+      <div className="slider">
+        <div className="control">
+          <div
+            className="btn left"
+            onClick={(event) => controlClickHandle(false)}
+          >
+            <IconArrowLeft width={77} height={77} />
+          </div>
+          <div
+            className="btn right"
+            onClick={(event) => controlClickHandle(true)}
+          >
+            <IconArrowRight width={77} height={77} />
+          </div>
+        </div>
+        <div className="picture">
+          <img src={pictureUrls[currentIndex]} alt="" />
+        </div>
+      </div>
+      <div className="preview"></div>
     </PictureBrowserWrapper>
   )
 })
--- src/base-ui/picture-browser/style.js
+++ src/base-ui/picture-browser/style.js
@@ -7,6 +7,10 @@ export const PictureBrowserWrapper = styled.div`
   right: 0;
   bottom: 0;
   left: 0;
+  display: flex;
+  flex-direction: column;
+
+  background-color: #333;
 
   .top {
     position: relative;
@@ -20,5 +24,53 @@ export const PictureBrowserWrapper = styled.div`
     }
   }
 
-  background-color: #333;
+  .slider {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    position: relative;
+
+    .control {
+      position: absolute;
+      z-index: 1;
+      left: 0;
+      right: 0;
+      top: 0;
+      display: flex;
+      justify-content: space-between;
+      bottom: 0;
+      color: #fff;
+
+      .btn {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 83px;
+        height: 100%;
+      }
+    }
+
+    .picture {
+      position: relative;
+      height: 100%;
+      overflow: hidden;
+      width: 100%;
+      max-width: 105vh;
+
+      img {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        margin: 0 auto;
+        height: 100%;
+        user-select: none;
+      }
+    }
+  }
+
+  .preview {
+    height: 100px;
+    margin-top: 10px;
+  }
 `
